f45bd32e4010a149d356ce555149f5a2cdc6f263
RinM <quinolactacin@gmail.com>
2025-10-31 23:41:19 +0200

    display calender

--- app/schedule/page.tsx
+++ app/schedule/page.tsx
@@ -1,5 +1,13 @@
+'use client'
+
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+
 export default function SchedulePage() {
   return (
-    <p>schedule</p>
-  );
+    <FullCalendar
+      plugins={[ dayGridPlugin ]}
+      initialView="dayGridMonth"
+    />
+  )
 }
