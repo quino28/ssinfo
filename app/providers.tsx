78954cc5461a9d365d573a175a99f67ce10c2811
RinM <quinolactacin@gmail.com>
2025-11-12 23:46:00 +0200

    fix styles when the system theme is light

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,15 +1,25 @@
 'use client'
 
 import { ThemeProvider } from 'next-themes'
+import { useEffect, useState } from "react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-    return (
-      <ThemeProvider
-        attribute="class"
-        defaultTheme='system'
-        enableSystem
-      >
-        {children}
-      </ThemeProvider>
-    );
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme='system'
+      enableSystem
+    >
+      {children}
+    </ThemeProvider>
+  );
 }
